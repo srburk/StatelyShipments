785d14a65f0256d9d33f5893ee6cc20ca258327e
Sam Burkhard <samryanburk@icloud.com>
2025-03-05 18:36:32 -0500

    Added sheet detent change animations, methods for changing them on transitions, and a much better searchable state picker. I need to remove some of the old deprecated functions in StatePickerButton next.

---
+++ StatelyShipments/ViewControllers/StatePickerViewController.h
@@ -0,0 +1,12 @@
+//
+//  StatePickerViewController.h
+//  StatelyShipments
+//
+//  Created by Sam Burkhard on 3/5/25.
+//
+
+#ifndef StatePickerViewController_h
+#define StatePickerViewController_h
+
+
+#endif /* StatePickerViewController_h */
---
+++ StatelyShipments/ViewControllers/StatePickerViewController.m
@@ -0,0 +1,8 @@
+//
+//  StatePickerViewController.m
+//  StatelyShipments
+//
+//  Created by Sam Burkhard on 3/5/25.
+//
+
+#import <Foundation/Foundation.h>
